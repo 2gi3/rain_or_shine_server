bd9450c3ba7f5a34a957344f6c1344606690576e
Giuseppe Ippolito <jo343f@icloud.com>
2025-10-08 21:24:52 +0100

    added CORS origin environment variable

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import express, { type Request, type Response } from "express";
 import userRoutes from './routes/user/index.js'
 import cors from "cors";
+import corsOptions from "./middleware/CORS/options.js";
 
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:8081"
-}));
+app.use(cors(corsOptions));
 
-// Middleware
+
 app.use(express.json());
 app.use('/users', userRoutes);
 
-// Routes
 app.get("/", (_req: Request, res: Response) => {
     res.send("Fudge!");
 });
---
+++ src/middleware/CORS/options.ts
@@ -0,0 +1,19 @@
+import type { CorsOptions } from "cors";
+
+const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+
+const localhostRegex = /^http:\/\/localhost:\d+$/;
+
+
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin) || localhostRegex.test(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Origin URL not included in allowedOrigins environment variable"));
+        }
+    }
+};
+export default corsOptions;
